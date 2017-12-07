d0c2441f609f8fa3810920635a3848e57ffb19e7
dunaevaa <32384871+dunaevaa@users.noreply.github.com>
2017-12-07 13:19:55 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -102,6 +102,7 @@ int main (int argc, char *argv[]) {
                             }
                         }
             }
+    menu ();
     cin >> choise;
     switch (choise) {
                     case 1: print_Matrix (Matrix, m, n)
